aa7a20eafdf79b80903b21436e7e7fdfa63b77ca
Kiran Nagar <KiranNagar@Kirans-MacBook-Pro.local>
2018-04-28 23:04:06 -0400

    Added components for the standings table and each team's roster

---
+++ project/src/components/Table.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="table-container" class="container">
+    <table>
+      <thead>
+        <tr>
+          <th>Position</th>
+          <th>    </th>
+          <th>Club</th>
+          <th>Played</th>
+          <th>Won</th>
+          <th>Drawn</th>
+          <th>Lost</th>
+          <th>GF</th>
+          <th>GA</th>
+          <th>GD</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entry in standing">
+          <td>{{ entry.position }}</td>
+          <td><img :src="{{ entry.crestURI }}" /></td>
+          <td>{{ entry.teamName }}</td>
+          <td>{{ entry.playedGames }}</td>
+          <td>{{ entry.wins }}</td>
+          <td>{{ entry.draws }}</td>
+          <td>{{ entry.losses }}</td>
+          <td>{{ entry.goals }}</td>
+          <td>{{ entry.goalsAgainst }}</td>
+          <td>{{ entry.goalDifference }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'table',
+  props: {
+    standing: {
+      type: Object,
+      required: true,
+    }
+  }
+};
+
+</script>
+
+<style lang="scss">
+</style>
---
+++ project/src/components/Team.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="squad-container" class="container">
+    <div id="team-photo" class="container">
+      <img :src="{{ team.crestURL }}" />
+    </div>
+    <h2>{{ team.name }}</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Name</th>
+          <th>Position</th>
+          <th>Nationality</th>
+          <th>DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="onePlayer in players">
+          <td>{{onePlayer.jerseyNumber}}</td>
+          <td>{{onePlayer.name}}</td>
+          <td>{{onePlayer.position}}</td>
+          <td>{{onePlayer.nationality}}</td>
+          <td>{{onePlayer.dateOfBirth}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'team',
+  props: {
+    team: {
+      type: Object,
+      required: true,
+    }
+  }
+};
+
+</script>
