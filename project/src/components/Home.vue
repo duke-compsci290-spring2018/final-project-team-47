f52113be328e66b902111c0ffe6cb020259767b8
Tara Nagar <tara.nagar@duke.edu>
2018-04-30 08:30:44 -0400

    Add project description

--- project/src/components/Home.vue
+++ project/src/components/Home.vue
@@ -1,7 +1,15 @@
 <template onresize="checkWidth">
 <div id="home" class="container">
     <h3>Project Description</h3>
-
+    <p class="home-description">Welcome to your one-stop-shop for all things Premier League Soccer! Sign up to keep track of your
+    favorite Premier League teams as they finish out the season. This website is designed with the fan in mind, so they never have
+    to worry about losing track of their #1 club.</p>
+    
+    <p class="home-description">As two people who have loved and played soccer all their lives, we knew we wanted to bring the soccer
+    community from all over the world together to create a special community. Soccer fans can get passionate, and we hope that is what
+    all of you do here, but we also hope that at the same time you will enjoy the company of your fellow passionate soccer fans.</p>
+    
+    <p class="home-description">Happy cheering!</p>
 </div>
 </template>
 
@@ -36,4 +44,9 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+    .home-description {
+        font-size: 16px;
+        padding-left: 50px;
+        padding-right: 50px;
+    }
 </style>
